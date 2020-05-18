d1ea71c89323c102907475647b5271b473bb9622
hwr <965479041@qq.com>
2020-05-18 14:18:02 +0800

    first-commit

--- test1.py
+++ test1.py
@@ -2,6 +2,15 @@ import requests
 
 
 if __name__ == '__main__':
-    r = requests.get("www.github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protoc-3.11.4-win64.zip")
-    with open("d:/test/protoc-3.11.4-win64.zip", "wb+") as code:
+    proxies = {
+        'http': "http://127.0.0.1:1080",
+        'https': "https://127.0.0.1:1080",
+    }
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36',
+    }
+    r = requests.get("https://download.vpngate.jp/common/cd.aspx/vpngate-client-2020.05.17-build-9745.147195.zip",
+                     timeout=10000, proxies=proxies, headers=headers)
+    with open("d:/test/vpngate-client-2020.05.17-build-9745.147195.zip", "wb+") as code:
         code.write(r.content)
